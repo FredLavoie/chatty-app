d841403f1c5304217a56d07b4d92281ce00da8f6
FredLavoie <flavoie3@gmail.com>
2019-07-03 19:02:11 +0000

    Add server capturing incoming messages

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -20,7 +20,11 @@ const wss = new SocketServer({ server });
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
+  ws.on('message', function incoming(data) {
+    let thing = JSON.parse(data);
+    console.log(`${thing.username} said: ${thing.content}`);
+    
+  });
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,40 @@ import Header from './Header.jsx';
 import {currentUser, messages} from './messages.json';
 import {generateRandomId} from '../utilities.js'
 
+
+
 class App extends Component {
   constructor() {
     super();
     this.state = { currentUser: currentUser, messages: messages };
     // this.state = { currentUser: {}, messages: [] };
+
+    // Creating the connection to the Socket Server
+    this.SocketServer = new WebSocket('ws://localhost:3001');
   }
 
+  handleOnOpen = event => {
+    console.log('Connection to server established.');
+    // changing from offline to online
+    // this.updateStatus(true);
+};
+
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ currentUser: currentUser, messages: messages })
-    }, 1000);
+
+    this.SocketServer.onopen = this.handleOnOpen;
+    // this.SocketServer.onmessage = this.handleOnMessage;
+    
+    // console.log("componentDidMount <App />");
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({ currentUser: currentUser, messages: messages })
+    // }, 1000);
+
   }
 
   addNewMessage = event => {
@@ -30,9 +46,12 @@ class App extends Component {
       let newContent = event.target.value;
       let id = generateRandomId();
       const newMessage = { type: "incomingMessage", content: newContent, username:this.state.currentUser.username , id: id };
-      const oldMessages = this.state.messages;
-      const allMessages = [...oldMessages, newMessage];
-      this.setState({ messages: allMessages });
+      // const oldMessages = this.state.messages;
+      // const allMessages = [...oldMessages, newMessage];
+
+      this.SocketServer.send(JSON.stringify(newMessage))
+
+      // this.setState({ messages: allMessages });
     }
   }
 
